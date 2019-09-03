350543e7acda706266fc5710fe730f8c4e6267d5
Prescott Blackler <Prescott@Prescotts-MacBook-Pro.local>
2019-09-03 14:33:07 -0400

    improved documentation and endpoints

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express'),
       morgan = require('morgan');
 const app = express();
 
-let toptenmovies = [{
+let movies = [{
   title: 'Test',
   director: 'Steven'
 }]
@@ -17,9 +17,43 @@ app.use(function(err, req, res, next) {
 })
 
 
-app.get('/movies', function(req, res) {
-  res.json(toptenmovies);
+app.get('/movies', (req, res) => {
+  res.json(movies);
 })
+
+app.get('/movies/:name', (req, res) => {
+  res.send("All data about 1 film");
+})
+
+app.get('/genres/:name', (req, res) => {
+  res.send("All films with this genre");
+})
+
+app.get('/directors/:name', (req, res) => {
+  res.send("Info about this Director");
+})
+
+app.post('/users', (req, res) => {
+  res.send("Register a new user");
+})
+
+app.put('/users/:name', (req, res) => {
+  res.send("Update user info")
+})
+
+app.post('/mylist', (req, res) => {
+  res.send("Add film to my favorites list")
+})
+
+app.delete('/mylist/:name', (req, res) => {
+  res.send("Remove a film from my favorites list")
+})
+
+app.delete('/users/:name', (req, res) => {
+  res.send("User Unregistered")
+})
+
+
 
 app.get('/', function(req, res) {
   res.send('This is my text!');
