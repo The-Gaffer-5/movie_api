baf75958b68b42aac489c42eea3667b2d17ebb2b
Prescott Blackler <Prescott@Prescotts-MacBook-Pro.local>
2019-09-17 14:39:15 -0400

    enable cors again

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ require('./passport');
 const { check, validationResult } = require('express-validator');
 
 
-var allowedOrigins = *;
+var allowedOrigins = [*];
 //mongoose.connect('mongodb://localhost:27017/myFlixDB', {useNewUrlParser: true});
 mongoose.connect('mongodb+srv://myFlixDBadmin:aq1SW@de3@myflixdb-gz5aj.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true});
 
