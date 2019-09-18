8fcd4a0430fc5101dd8d23fb058187d0a0568b3c
Prescott Blackler <Prescott@Prescotts-MacBook-Pro.local>
2019-09-18 12:41:00 -0400

    init commit

---
+++ client/src/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { MainView } from './components/main-view/main-view';
+//import { MovieCard } from '../movie-card/movie-card';
+
+import './index.scss';
+
+class MyFlixApplication extends React.Component {
+  render() {
+    return <MainView />;
+  }
+}
+
+const container = document.getElementsByClassName('app-container')[0];
+
+ReactDOM.render(React.createElement(MyFlixApplication), container)
