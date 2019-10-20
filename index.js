bc8998a978e7cfeda6a9e1590662e92fb226476d
Prescott Blackler <Prescott@Prescotts-MacBook-Pro.local>
2019-10-20 12:40:02 -0400

    new error

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.get("/client/*", (req, res) => {
 app.use(morgan('common'));
 app.use(function (err, req, res, next) {
   console.error(err.stack);
-  res.status(500).send('We\'re going down!')
+  res.status(500).send('Error: ' + err)
 })
 
 
