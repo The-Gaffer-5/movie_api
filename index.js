f8ba1800cab31c35cae896e14044b9a1fad25ac3
Prescott Blackler <Prescott@Prescotts-MacBook-Pro.local>
2019-10-21 17:50:57 -0400

    again

--- index.js
+++ index.js
@@ -48,13 +48,12 @@ app.use(express.static('public'));
 
 
 if (process.env.NODE_ENV === 'production') {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/dist')));
-  // Handle React routing, return all requests to React app
-  app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'client/dist', 'index.html'));
-  });
+  app.use(express.static('client/build'));
 }
+app.get('*', (request, response) => {
+  response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
 
 
 app.use(morgan('common'));
