9bade7378e61f347524d3756c7e03f6012c7e76b
Prescott Blackler <Prescott@Prescotts-MacBook-Pro.local>
2019-10-21 18:16:55 -0400

    change cors

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const { check, validationResult } = require('express-validator');
 
 // var allowedOrigins = ['http://localhost:1234']
 
-var allowedOrigins = ['https://prescottflixapp.herokuapp.com']
+// var allowedOrigins = ['https://prescottflixapp.herokuapp.com']
 
 //mongoose.connect('mongodb://localhost:27017/myFlixDB', {useNewUrlParser: true});
 mongoose.connect('mongodb+srv://myFlixDBadmin:aq1SW@de3@myflixdb-gz5aj.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true });
@@ -24,17 +24,22 @@ mongoose.connect('mongodb+srv://myFlixDBadmin:aq1SW@de3@myflixdb-gz5aj.mongodb.n
 
 const Movies = Models.Movie;
 const Users = Models.User;
+//CORS
+// app.use(cors({
+//   origin: function (origin, callback) {
+//     if (!origin) return callback(null, true);
+//     if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+//       var message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+//       return callback(new Error(message), false);
+//     }
+//     return callback(null, true);
+//   }
+// }));
 
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
-      var message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message), false);
-    }
-    return callback(null, true);
-  }
-}));
+app.use(cors());
+
+
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
